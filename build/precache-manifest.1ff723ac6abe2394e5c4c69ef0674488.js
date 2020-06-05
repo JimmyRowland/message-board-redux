b455811802a549f96468c4dc481c0eb6ac78eb48
Jimmy Rowland <tian_yu@student.smc.edu>
2020-06-04 21:01:18 -0700

    relative path

---
+++ build/precache-manifest.1ff723ac6abe2394e5c4c69ef0674488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea182c3aeb8616df42c0e0c7d7756a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebca50fb542addca9066",
+    "url": "./static/css/main.04eef6a8.chunk.css"
+  },
+  {
+    "revision": "79e6220b0284f94a0c7a",
+    "url": "./static/js/2.a513dad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a513dad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebca50fb542addca9066",
+    "url": "./static/js/main.9b2e3d8f.chunk.js"
+  },
+  {
+    "revision": "ca7910fa757583482b96",
+    "url": "./static/js/runtime-main.626f48d2.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.112244571cdab93037a1c1b70a857fea.js"
+  "./precache-manifest.1ff723ac6abe2394e5c4c69ef0674488.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
