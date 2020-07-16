4102fe80a4c0e44296e0ec94219ae43989c61046
Jimmy Rowland <tian_yu@student.smc.edu>
2020-07-15 22:31:48 -0700

    module bug

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,10 @@
 import "dotenv/config"
 import {connection, connect} from "mongoose";
 import express from "express";
+import cors from "cors";
 import routes from "./routes/index"
-import * as path from "path";
 
 (async()=>{
-    const PORT = process.env.PORT || 8080
     const app = express();
     const devconnection: string|undefined = process.env.DB_STRING;
     if(devconnection){
@@ -17,15 +16,14 @@ import * as path from "path";
         //middleware
         app.use(express.json());
         app.use(express.urlencoded({extended: true}));
-        if (process.env.NODE_ENV === 'production'){
-            app.use(express.static('client.build/'));
-            app.get("*", (req,res)=>{
-                res.sendFile(path.resolve(__dirname,"client",'build','index.html'))
-            })
-        }
+        app.use(
+            cors({
+                origin: "http://localhost:3000",
+            }),
+        );
         // controller
         app.use(routes);
-        app.listen(8080, () => {
+        app.listen(4000, () => {
             console.log("express server started on http://localhost:4000");
         });
 
