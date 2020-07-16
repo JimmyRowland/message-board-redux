8488d0039186f24ced82222523ace581f7c796b1
Jimmy Rowland <tian_yu@student.smc.edu>
2020-07-15 22:03:20 -0700

    heroku

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 import "dotenv/config"
 import {connection, connect} from "mongoose";
 import express from "express";
-import cors from "cors";
 import routes from "./routes/index"
+import * as path from "path";
 
 (async()=>{
+    const PORT = process.env.PORT || 8080
     const app = express();
     const devconnection: string|undefined = process.env.DB_STRING;
     if(devconnection){
@@ -16,14 +17,15 @@ import routes from "./routes/index"
         //middleware
         app.use(express.json());
         app.use(express.urlencoded({extended: true}));
-        app.use(
-            cors({
-                origin: "http://localhost:3000",
-            }),
-        );
+        if (process.env.NODE_ENV === 'production'){
+            app.use(express.static('client.build/'));
+            app.get("*", (req,res)=>{
+                res.sendFile(path.resolve(__dirname,"client",'build','index.html'))
+            })
+        }
         // controller
         app.use(routes);
-        app.listen(4000, () => {
+        app.listen(8080, () => {
             console.log("express server started on http://localhost:4000");
         });
 
